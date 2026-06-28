85d3d98af218469f31a79003bf1475d2870bf77d
study-NY <shij24634@gmail.com>
2026-06-28 11:17:42 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "target-study-v8-lesson-ui";
+const CACHE_NAME = 'target-study-v8.1-1';
 const ASSETS = ["./?v=8","./index.html?v=8","./data.js?v=8","./app.js?v=8","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install", event => { self.skipWaiting(); event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS).catch(()=>{}))); });
 self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); self.clients.claim(); });
