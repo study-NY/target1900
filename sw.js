5cdb6ad16780e4431232c1ca011c0df16fa113ab
study-NY <shij24634@gmail.com>
2026-06-28 10:34:48 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,2 +1,5 @@
-self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('fetch',e=>{});
+const CACHE_NAME = "target-study-v7";
+const ASSETS = ["./?v=7","./index.html?v=7","./data.js?v=7","./app.js?v=7","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+self.addEventListener("install", event => { self.skipWaiting(); event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS).catch(()=>{}))); });
+self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); self.clients.claim(); });
+self.addEventListener("fetch", event => { event.respondWith(fetch(event.request).then(res => { const copy = res.clone(); caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(()=>{}); return res; }).catch(() => caches.match(event.request))); });
