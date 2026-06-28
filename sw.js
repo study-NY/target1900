f5b59f596b92c3eae6ff003d042ff038a06e35c5
study-NY <shij24634@gmail.com>
2026-06-28 10:53:19 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "target-study-v7-rebuilt";
-const ASSETS = ["./?v=7","./index.html?v=7","./data.js?v=7","./app.js?v=7","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+const CACHE_NAME = "target-study-v8-lesson-ui";
+const ASSETS = ["./?v=8","./index.html?v=8","./data.js?v=8","./app.js?v=8","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install", event => { self.skipWaiting(); event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS).catch(()=>{}))); });
 self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); self.clients.claim(); });
 self.addEventListener("fetch", event => { event.respondWith(fetch(event.request).then(res => { const copy = res.clone(); caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(()=>{}); return res; }).catch(() => caches.match(event.request))); });
