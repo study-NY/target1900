e4f7d03ce4b08479a960224afe516620a48a4e17
study-NY <shij24634@gmail.com>
2026-07-01 22:40:24 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'target-study-v8.2-1';
-const ASSETS = ["./?v=8.2","./index.html?v=8.2","./data.js?v=8.2","./app.js?v=8.2","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
-self.addEventListener("install", event => { self.skipWaiting(); event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS).catch(()=>{}))); });
-self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); self.clients.claim(); });
-self.addEventListener("fetch", event => { event.respondWith(fetch(event.request).then(res => { const copy = res.clone(); caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(()=>{}); return res; }).catch(() => caches.match(event.request))); });
+const CACHE_NAME = 'target-study-v9';
+const ASSETS = ['./','./index.html?v=9','./app.js?v=9','./data.js?v=9','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install', event => { self.skipWaiting(); event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS).catch(()=>{}))); });
+self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch', event => { event.respondWith(fetch(event.request).then(res => { const copy=res.clone(); caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy)).catch(()=>{}); return res; }).catch(() => caches.match(event.request))); });
